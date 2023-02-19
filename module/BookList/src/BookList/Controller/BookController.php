e8e9b6e1da54aa12015aa80c424c0c55deaabea9
Ahmed Ch <ahmedchouihi@gmail.com>
2023-02-19 14:39:38 +0100

    updatingTheIndexView

--- module/BookList/src/BookList/Controller/BookController.php
+++ module/BookList/src/BookList/Controller/BookController.php
@@ -1,11 +1,14 @@
 <?php
 namespace BookList\Controller;
+use Zend\View\Model\ViewModel;
 use Zend\Mvc\Controller\AbstractActionController;
 
 class BookController extends AbstractActionController{
     public function indexAction()
     {
-
+        return new ViewModel(array(
+            'books' => []
+        ));
     }
     public function addAction(){
 
