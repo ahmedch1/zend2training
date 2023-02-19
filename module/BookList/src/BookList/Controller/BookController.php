ad451600309af83e116556fe88876f5758e842d1
Ahmed Ch <ahmedchouihi@gmail.com>
2023-02-19 14:00:09 +0100

    CreatingControllerViews

---
+++ module/BookList/src/BookList/Controller/BookController.php
@@ -0,0 +1,21 @@
+<?php
+namespace BookList\Controller;
+use Zend\Mvc\Controller\AbstractActionController;
+
+class BookController extends AbstractActionController{
+    public function indexAction()
+    {
+
+    }
+    public function addAction(){
+
+    }
+    public function editAction(){
+
+    }
+    public function deleteAction(){
+
+    }
+
+}
+
