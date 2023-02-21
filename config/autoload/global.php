f63dc61947199d6a4b364625997557a5a4dbe0b1
Ahmed Ch <ahmedchouihi@gmail.com>
2023-02-21 11:54:33 +0100

    ConfiguringTheDbConnection

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,19 @@
  */
 
 return array(
+    'db'=>array(
+        'driver'=>'Pdo',
+        'dsn'=>
+            'mysql:dbname=book_list_training;host=127.0.0.1',
+        'driver_options'=>array(
+            PDO::MYSQL_ATTR_INIT_COMMAND=>'SET NAMES \'UTF8\''
+        ),
+    ),
+    'service_manager'=>array(
+        'factories'=>array(
+            'Zend\Db\Adapter\Adapter'
+            =>'Zend\Db\Adapter\AdapterServiceFactory'
+        )
+    )
     // ...
 );
