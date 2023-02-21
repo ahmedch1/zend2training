b9eff7c1abc5cdf286800177cb3f588ff97e696f
Ahmed Ch <ahmedchouihi@gmail.com>
2023-02-21 15:30:15 +0100

    updatingTheHomeRoute

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -17,7 +17,7 @@ return array(
                 'options' => array(
                     'route'    => '/',
                     'defaults' => array(
-                        'controller' => 'Application\Controller\Index',
+                        'controller' => 'BookList\Controller\Book',
                         'action'     => 'index',
                     ),
                 ),
